c4f6bda8b5013d2ed756f400e694ace59bb49d6b
cjpelaez <cjpelaez@querry.local>
2018-06-25 09:44:10 +0200

    New entry
    Added new entry
    Error page corrections
    Change JS script to avoid edit all entries each time a new one is added
    Add bold fonts to avoid bad rendering
    Change scroll icon to font

--- js/scroll.js
+++ js/scroll.js
@@ -7,8 +7,8 @@ var scrolltotop = {
     //startline: Integer. Number of pixels from top of doc scrollbar is scrolled before showing control
     //scrollto: Keyword (Integer, or "Scroll_to_Element_ID"). How far to scroll document up when control is clicked on (0=top).
     setting: {startline:100, scrollto: 0, scrollduration:1000, fadeduration:[500, 100]},
-    controlHTML: '<img src="images/up.png"/>', //HTML for control, which is auto wrapped in DIV w/ ID="topcontrol"
-    controlattrs: {offsetx:15, offsety:15}, //offset of control relative to right/ bottom of window corner
+    controlHTML: '<i class="fa fa-chevron-circle-up" style="font-size:45px; color: #28282890;"></i>', //HTML for control, which is auto wrapped in DIV w/ ID="topcontrol"
+    controlattrs: {offsetx:15, offsety:10}, //offset of control relative to right/ bottom of window corner
     anchorkeyword: '#top', //Enter href value of HTML anchors on the page that should also act as "Scroll Up" links
     state: {isvisible:false, shouldvisible:false},
 
