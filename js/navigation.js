4143684798bfd07f0f8dce8bfb2ed01c516ea0db
Carlos J. Peláez Rivas <carlospelaez3992@gmail.com>
2018-10-30 20:39:38 +0100

    Change date in new entry.

--- js/navigation.js
+++ js/navigation.js
@@ -4,7 +4,7 @@ var pages = [
 	'md_2014_05_02.html', 'md_2014_06_10.html', 'md_2014_08_07.html', 'md_2014_10_01.html',
 	'md_2015_01_12.html', 'md_2015_08_10.html', 'md_2015_08_17.html', 'md_2015_08_24.html',
 	'md_2016_09_22.html',
-	'md_2018_03_21.html', 'md_2018_05_04.html', 'md_2018_10_18.html'
+	'md_2018_03_21.html', 'md_2018_05_04.html', 'md_2018_10_30.html'
 ];
 
 var split = new URL(window.location.href).pathname.split('/');
