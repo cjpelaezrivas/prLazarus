67ea219ebf5bc9a8a1044a13e048782eb2227f5e
cjpelaez <cjpelaez@querry.local>
2018-12-04 16:09:16 +0100

    Some corrections
    Corrections in quotes in label
    Correct how progress bar is calculated al load page
    Show progress bar in Firefox
    Change some link styles

--- js/progressBar.js
+++ js/progressBar.js
@@ -1,9 +1,14 @@
+function calculateProgressBarWidth() {
+	var wintop = $(window).scrollTop();
+	var winheight = $(window).height();
+	var docheight = $('.main-container').height();
+	var totalScroll = (wintop / (docheight - winheight)) * 100;
+
+	$(".progressBar").css("width", totalScroll + "%");
+}
+
 $(window).load(function(){
-	$(window).scroll(function() {
-		var wintop = $(window).scrollTop();
-		var winheight = $(window).height();
-		var docheight = $('.main-container').height();
-		var totalScroll = (wintop / (docheight - winheight)) * 100;
-		$(".progressBar").css("width",totalScroll + "%");
-	});
+	$(window).scroll(calculateProgressBarWidth);
 });
+
+calculateProgressBarWidth();
