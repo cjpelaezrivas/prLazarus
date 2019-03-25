2cdb3a26527ad04fdde6fc957a6a5a62338d64f3
cjpelaez <cjpelaez@querry.local>
2019-03-25 15:58:01 +0100

    Filter modification and new entry
    Modify filter to hide year when no result are found in that year.
    Add new entry.

--- js/filter.js
+++ js/filter.js
@@ -25,22 +25,62 @@ function filterByTagParameter() {
 	}
 }
 
-function filter(search, c) {
-	var entries = $(".entry");
-	for (var i = 0; i < entries.length; i++) {
-		var tags = $(entries[i]).find('.' + c);
 
-		if(!tags.html() || !tags.html().toUpperCase().includes(search)) {
-			entries[i].style.display = 'none';
+function filter(search, c) {
+	var someVisible = false;
+
+	var years = $('.year');
+	for(var i = 0; i < years.length; i++) {
+
+		var yearVisible = false;
+
+		var entries = $(years[i]).find('.entry')
+		for (var j = 0; j < entries.length; j++) {
+
+			var tags = $(entries[j]).find('.' + c);
+
+			if(tags.html() && tags.html().toUpperCase().includes(search)) {
+				yearVisible = true;
+			} else {
+				entries[j].style.display = 'none';
+			}
 		}
+
+		if(yearVisible) {
+			someVisible = true;
+		} else {
+			years[i].style.display = 'none';
+		}
+	}
+
+	if(!someVisible) {
+		$('.noResult')[0].style.display = 'block';
 	}
 }
 
+// function filter(search, c) {
+// 	var entries = $(".entry");
+// 	for (var i = 0; i < entries.length; i++) {
+// 		var tags = $(entries[i]).find('.' + c);
+
+// 		if(!tags.html() || !tags.html().toUpperCase().includes(search)) {
+// 			entries[i].style.display = 'none';
+// 		}
+// 	}
+// }
+
 function removeFilter() {
+	var years = $(".year");
+	for (var i = 0; i < years.length; i++) {
+		years[i].style.display = 'initial';
+	}
+
 	var entries = $(".entry");
 	for (var i = 0; i < entries.length; i++) {
 		entries[i].style.display = 'initial';
 	}
+
+	$('.noResult')[0].style.display = 'none';
 }
 
 function getParameter(parameter) {
